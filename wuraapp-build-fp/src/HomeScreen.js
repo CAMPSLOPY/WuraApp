9e1b0e7f3166d1dff489c39dff67907e18556c93
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-16 01:04:10 +0100

    added the banner component to the home screen

--- wuraapp-build-fp/src/HomeScreen.js
+++ wuraapp-build-fp/src/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../src/HomeScreen.css";
 import Nav from './Nav';
+import Banner from '../src/Banner.js';
 function HomeScreen() {
     return (
       <div className="homeScreen">
