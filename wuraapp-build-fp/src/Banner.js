37698b3f0cba4588c356ff564b5692764e748abd
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-16 21:37:44 +0100

    banner

--- wuraapp-build-fp/src/Banner.js
+++ wuraapp-build-fp/src/Banner.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import "./Banner.css";
 function Banner() {
+
+    function truncate(string, n){
+return string?.length > n ? string.substring(0, n - 1)+ "..." : string;
+
+    }
     return (
         <header className="banner" style={{
-            backgroundImage: `url("https://wallpapercave.com/wp/wp1917118.jpg")`,
+            backgroundImage: `url("https://www.shutterstock.com/image-vector/humpolec-czech-republic-january-23-260nw-2272947519.jpg")`,
             backgroundSize: "cover",
             backgroundPosition: "center center"
         }}>
-            
+            <div className="banner_contents">
+                <h1 className="banner_title">MovieName</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_desc">
+                    This is a test Description
+                </h1>
+            </div>
+            <div className="banner_fade"></div>
         </header>
     );
 }
