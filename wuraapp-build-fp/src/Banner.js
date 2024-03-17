daffd44439acb630b3afaad5f9ca144a9cef9c61
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-17 02:08:59 +0100

    Banner compt

--- wuraapp-build-fp/src/Banner.js
+++ wuraapp-build-fp/src/Banner.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Request";
+
+
+
+
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fecthData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ]);
+            return request;
+        }
+        fecthData();
+    },[])
+
+console.log(movie)
 
     function truncate(string, n){
 return string?.length > n ? string.substring(0, n - 1)+ "..." : string;
@@ -8,21 +28,22 @@ return string?.length > n ? string.substring(0, n - 1)+ "..." : string;
     }
     return (
         <header className="banner" style={{
-            backgroundImage: `url("https://www.shutterstock.com/image-vector/humpolec-czech-republic-january-23-260nw-2272947519.jpg")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundSize: "cover",
-            backgroundPosition: "center center"
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
         }}>
             <div className="banner_contents">
-                <h1 className="banner_title">MovieName</h1>
+                <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
                 <h1 className="banner_desc">
-                    This is a test Description
+                    {truncate(movie?.overview, 200)}
                 </h1>
             </div>
-            <div className="banner_fade"></div>
+            <div className="banner_fade"/>
         </header>
     );
 }
