6ff18236e6a0b4ed891305a01a821ef92a930c86
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-20 00:50:31 +0100

    main app

--- wuraapp-build-fp/src/App.js
+++ wuraapp-build-fp/src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
-
-
+import HomeScreen from './screens/HomeScreen';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} 
-from "react-router-dom";
+BrowserRouter as Router,Route, Routes} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-          
+      
 <Router>
-<switch>
-  <Router path="/test">
-    <h1>wow whats up</h1>
-  </Router>
-<Route path="">
-<HomeScreen/>
-</Route>
-
-</switch>
-
+<Routes>
+<Route path='/' exact Component={HomeScreen}/>
+</Routes>
 </Router>
-
-
     </div>
   );
 }
