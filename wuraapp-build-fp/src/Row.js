a39e4113636f57bcd4b48c3505ffa4b9b70921cd
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-22 02:11:20 +0100

    row component

--- wuraapp-build-fp/src/Row.js
+++ wuraapp-build-fp/src/Row.js
@@ -1,42 +1,41 @@
-import React, { useEffect , useState} from 'react';
-import  "./Row.css";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
 import axios from "./axios.js";
 
-
-function Row({title, fetchUrl, isLargeRow = false}) {
-    const [movies, setMovies] = useState([]);
-    const base_url = "https://image.tmdb.org/t/p/original/";
-    useEffect(()=>{
-        async function fecthData(){
-
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fecthData();
-    },[fetchUrl]);
-console.log(movies);
-    return (
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fecthData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fecthData();
+  }, [fetchUrl]);
+  console.log(movies);
+  return (
     <div className="row">
-            <h2>{title}</h2>
+      <h2>{title}</h2>
 
-         <div className="row_posters">
-            {movies.map((movie) =>(
-                ((isLargeRow && movie.poster_path) 
-                || (!isLargeRow && movie.backdrop_path)) &&(
-                    <img className={`row_poster ${isLargeRow  && "row_posterLarge"}`} 
-                    key={movie.id}
-                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                    alt={movie.name} 
-                    />
-
-                )
-              
-            ))}
-
-     </div>   
+      <div className="row_posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
     </div>
-    );
+  );
 }
 
 export default Row;
