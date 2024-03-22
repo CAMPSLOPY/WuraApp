329f8cc788fa2c60517491a7902c9f62985c29f4
CAMPSLOPY <shobowaletola@gmail.com>
2024-03-22 02:08:39 +0100

    fb

--- wuraapp-build-fp/src/firebase.js
+++ wuraapp-build-fp/src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB1aKX5_fPTuH0QIKa56Ls1ZkBaD_ctBhw",
-    authDomain: "wuraapp-build-fp.firebaseapp.com",
-    projectId: "wuraapp-build-fp",
-    storageBucket: "wuraapp-build-fp.appspot.com",
-    messagingSenderId: "153662245122",
-    appId: "1:153662245122:web:2b18829dd021d2ca89da1b"
-  };
+  apiKey: "AIzaSyB1aKX5_fPTuH0QIKa56Ls1ZkBaD_ctBhw",
+  authDomain: "wuraapp-build-fp.firebaseapp.com",
+  projectId: "wuraapp-build-fp",
+  storageBucket: "wuraapp-build-fp.appspot.com",
+  messagingSenderId: "153662245122",
+  appId: "1:153662245122:web:2b18829dd021d2ca89da1b",
+};
